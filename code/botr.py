29ef76e1213d2489d38aba4b6310b01931727db3
SohKey <45180021+SohKey@users.noreply.github.com>
2022-02-05 11:30:38 +0100

    Correction de BUG pour linux

--- code/botr.py
+++ code/botr.py
@@ -123,11 +123,8 @@ while x != 1:
         API_REDDIT_USER_AGENT = eval(cfg[compte]["API_REDDIT_USER_AGENT"])
         API_REDDIT_PASSWORD = eval(cfg[compte]["API_REDDIT_PASSWORD"])
     except:
-        if(my_os == "linux" or "Linux"):
-            x=1
-        else:
-            print(f"{Fore.RED}[{Fore.LIGHTWHITE_EX}!{Fore.RED}] {Fore.LIGHTRED_EX}Saisie invalide ! {fontS.END}")
-            print(f"{Fore.RED}__"*60)
+        print(f"{Fore.RED}[{Fore.LIGHTWHITE_EX}!{Fore.RED}] {Fore.LIGHTRED_EX}Saisie invalide ! {fontS.END}")
+        print(f"{Fore.RED}__"*60)
     else:
         x = 1
 
